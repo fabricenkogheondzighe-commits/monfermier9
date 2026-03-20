7fa44db4743dce4a0b3983a1cc90bf7fcaa1fa45
fabricenkogheondzighe-commits <265939824+fabricenkogheondzighe-commits@users.noreply.github.com>
2026-03-20 17:54:23 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ sw.js
@@ -0,0 +1,38 @@
+var CACHE = 'monfermier-v20260319';
+var FILES = ['./', './index.html'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c){ return c.addAll(FILES); }).catch(function(){})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE; })
+          .map(function(k){ return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(function(res){
+      var clone = res.clone();
+      caches.open(CACHE).then(function(c){ c.put(e.request, clone); });
+      return res;
+    }).catch(function(){
+      return caches.match(e.request);
+    })
+  );
+});
+
+self.addEventListener('message', function(e){
+  if(e.data === 'skipWaiting') self.skipWaiting();
+});
