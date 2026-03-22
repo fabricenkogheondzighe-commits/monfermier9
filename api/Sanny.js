074a779477d1098d6d8d0d8e314775cf9cafa7bb
fabricenkogheondzighe-commits <fabricenkogheondzighe@gmail.com>
2026-03-23 00:04:12 +0100

    Update Sanny.js

--- api/Sanny.js
+++ api/Sanny.js
@@ -1,4 +1,5 @@
-export default async function handler(req, res) {
+
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -24,4 +25,3 @@ export default async function handler(req, res) {
     res.status(500).json({ error: error.message });
   }
 }
-Cliquez "Commit changes
